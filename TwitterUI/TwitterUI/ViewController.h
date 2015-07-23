db2d2ffc8ec7b92031bbef6b2b359ed0c5d8fea7
shena yoshida <sheyoshida@gmail.com>
2015-07-22 21:52:26 -0400

    first draft

--- TwitterUI/TwitterUI/ViewController.h
+++ TwitterUI/TwitterUI/ViewController.h
@@ -8,7 +8,14 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+
+@interface ViewController : UIViewController {
+    
+    __weak IBOutlet UIButton *followButton;
+    
+    
+
+}
 
 
 @end
--- TwitterUI/TwitterUI/ViewController.m
+++ TwitterUI/TwitterUI/ViewController.m
@@ -7,16 +7,42 @@
 //
 
 #import "ViewController.h"
+#import <QuartzCore/QuartzCore.h> // added this to round corners!!
+
 
 @interface ViewController ()
+
+@property (weak, nonatomic) IBOutlet UIImageView *avatarPic;
+@property (weak, nonatomic) IBOutlet UIView *avatarView;
+@property (weak, nonatomic) IBOutlet UIView *followView;
+@property (weak, nonatomic) IBOutlet UIView *bubbleView;
+@property (weak, nonatomic) IBOutlet UIView *innerFollowView;
+@property (weak, nonatomic) IBOutlet UIView *innerBubbleView;
+@property (weak, nonatomic) IBOutlet UIImageView *bubbleImage;
 
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
+    
     [super viewDidLoad];
+    
+    self.avatarPic.layer.cornerRadius = 20.0;
+    self.avatarView.layer.cornerRadius = 20.0;
+    self.avatarPic.clipsToBounds = YES; // this sticks the view to the image
+    
+    self.followView.layer.cornerRadius = 5.0;
+    self.innerFollowView.layer.cornerRadius = 5.0;
+    
+    self.bubbleView.layer.cornerRadius = 5.0;
+    
+    self.innerBubbleView.layer.cornerRadius = 5.0;
+    self.bubbleImage.layer.cornerRadius = 5.0;
+    self.bubbleImage.clipsToBounds = YES;
+    
     // Do any additional setup after loading the view, typically from a nib.
+    
 }
 
 - (void)didReceiveMemoryWarning {
