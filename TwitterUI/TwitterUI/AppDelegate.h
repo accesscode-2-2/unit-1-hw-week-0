7bd829bef3c650a8695532001e1d1e9148307d0d
shena yoshida <sheyoshida@gmail.com>
2015-07-24 16:26:37 -0400

    homework assignment

--- TwitterUI/TwitterUI/AppDelegate.h
+++ TwitterUI/TwitterUI/AppDelegate.h
@@ -3,6 +3,7 @@
 //  TwitterUI
 //
 //  Created by Michael Kavouras on 7/19/15.
+//  Edited by Shena Yoshida 🙋🏻on 7/23/15/.
 //  Copyright (c) 2015 Mike Kavouras. All rights reserved.
 //
 
--- TwitterUI/TwitterUI/ViewController.m
+++ TwitterUI/TwitterUI/ViewController.m
@@ -3,6 +3,7 @@
 //  TwitterUI
 //
 //  Created by Michael Kavouras on 7/19/15.
+//  Edited by Shena Yoshida 🙋🏻on 7/23/15/.
 //  Copyright (c) 2015 Mike Kavouras. All rights reserved.
 //
 
@@ -18,7 +19,10 @@
 @property (weak, nonatomic) IBOutlet UIView *bubbleView;
 @property (weak, nonatomic) IBOutlet UIView *innerFollowView;
 @property (weak, nonatomic) IBOutlet UIView *innerBubbleView;
-@property (weak, nonatomic) IBOutlet UIImageView *bubbleImage;
+@property (weak, nonatomic) IBOutlet UIImageView *imageBubble;
+@property (weak, nonatomic) IBOutlet UIImageView *imageFollow;
+
+
 
 @end
 
@@ -28,22 +32,35 @@
     
     [super viewDidLoad];
     
+    // Here we round corners:
+    
     self.avatarPic.layer.cornerRadius = 20.0;
     self.avatarView.layer.cornerRadius = 20.0;
     self.avatarPic.clipsToBounds = YES; // this sticks the view to the image
+    
+    // Alternately, I could have added a border with code, like this:
+    
+    //    self.avatarPic.layer.borderColor = [UIColor whiteColor].CGColor;
+    //    self.avatarPic.layer.borderWidth = 5.0;
     
     self.followView.layer.cornerRadius = 5.0;
     self.innerFollowView.layer.cornerRadius = 5.0;
     
     self.bubbleView.layer.cornerRadius = 5.0;
-    
     self.innerBubbleView.layer.cornerRadius = 5.0;
-    self.bubbleImage.layer.cornerRadius = 5.0;
-    self.bubbleImage.clipsToBounds = YES;
     
-    // Do any additional setup after loading the view, typically from a nib.
     
-}
+    // I change the image color from black to blue with this code:
+    
+    self.imageFollow.image = [self.imageFollow.image imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate];
+    
+    [self.imageFollow setTintColor:[UIColor colorWithRed:85.0/255.0 green:172.0/255.0 blue:238.0/255.0 alpha:1]];
+    
+    self.imageBubble.image = [self.imageBubble.image imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate];
+    
+    [self.imageBubble setTintColor:[UIColor colorWithRed:85.0/255.0 green:172.0/255.0 blue:238.0/255.0 alpha:1]];
+    
+    }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
