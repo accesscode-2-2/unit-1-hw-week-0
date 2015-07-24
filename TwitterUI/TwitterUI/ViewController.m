aa7d31b5f59b233481527944601ab63cacdffc40
Mesfin <mesbekmek@gmail.com>
2015-07-24 16:18:51 -0400

    Final commit

--- TwitterUI/TwitterUI/ViewController.m
+++ TwitterUI/TwitterUI/ViewController.m
@@ -7,21 +7,66 @@
 //
 
 #import "ViewController.h"
+#import <QuartzCore/QuartzCore.h>
+#import "UIImage+remote.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIImageView *profile;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *segmentedControl;
+
 
 @end
 
 @implementation ViewController
+- (IBAction)settingsButton:(UIButton *)sender {
+    NSLog(@"Settings Button tapped");
+}
+
+- (IBAction)messageButton:(UIButton *)sender {
+    NSLog(@"Message Button tapped");
+}
+
+- (IBAction)followButton:(UIButton *)sender {
+    NSLog(@"Folow Button tapped");
+}
+
+- (IBAction)userButton:(UIButton *)sender {
+    NSLog(@"User Button tapped");
+}
+
+- (IBAction)notificationButton:(UIButton *)sender {
+    NSLog(@"Notification Button tapped");
+}
+
+- (IBAction)homeButton:(UIButton *)sender {
+    NSLog(@"Home Button tapped");
+}
+
+- (IBAction)messageButton2:(UIButton *)sender {
+        NSLog(@"Message Button 2 tapped");
+}
+
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.profile.layer.masksToBounds = YES;
+    
+    self.profile.layer.cornerRadius = 10.0;
+    
+    [self.profile.layer setBorderWidth:2.0f];
+    [self.profile.layer setBorderColor:[UIColor whiteColor].CGColor];
+    
+    
+//    if(self.segmentedControl.selectedSegmentIndex == 0){
+//        self.segmentedControl.backgroundColor = [UIColor colorWithRed:0 green:172 blue:237.292 alpha:1];
+//    }
+
+    
+    self.profile.image = [UIImage imageFromURLString:@"https://pbs.twimg.com/profile_images/607607468856770561/lkMPz-Lu_400x400.jpg"];
+    
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
+
 
 @end
---
+++ TwitterUI/UIImage+remote.h
@@ -0,0 +1,15 @@
+//
+//  UIImage+remote.h
+//  TwitterUI
+//
+//  Created by Mesfin Bekele Mekonnen on 7/23/15.
+//  Copyright (c) 2015 Mike Kavouras. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (remote)
+
++ (UIImage *)imageFromURLString:(NSString *)string;
+
+@end
