752ad3b9774c66d4af580aa56722ea0e11367de6
Jamaal <jsedayao@gmail.com>
2015-07-24 20:53:08 -0400

    submit

--- TwitterUI/TwitterUI/ViewController.m
+++ TwitterUI/TwitterUI/ViewController.m
@@ -10,9 +10,10 @@
 #import <QuartzCore/QuartzCore.h>
 
 @interface ViewController ()
-@property (weak, nonatomic) IBOutlet UIView *followBar;
-@property (weak, nonatomic) IBOutlet UISegmentedControl *segmentedControl;
 @property (weak, nonatomic) IBOutlet UILabel *labelTweets;
+@property (weak, nonatomic) IBOutlet UILabel *labelMedia;
+@property (weak, nonatomic) IBOutlet UILabel *labelFavorites;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *segmentedControl;
 
 @end
 
@@ -20,13 +21,35 @@
 
 - (void)viewDidLoad {
     
-    self.followBar.layer.borderColor = [UIColor colorWithRed: 0.34f green: 0.67f blue: 0.92f alpha: 1.0f].CGColor;
+//    self.followBar.layer.borderColor = [UIColor colorWithRed: 0.34f green: 0.67f blue: 0.92f alpha: 1.0f].CGColor;
     
     [super viewDidLoad];
 }
-- (IBAction)segmentControlValueDidChange:(UISegmentedControl *)sender {
+- (IBAction)segmentedControlAction:(UISegmentedControl *)sender {
+    UISegmentedControl *segmentedControl = (UISegmentedControl *) sender;
+    NSInteger selectedSegment = segmentedControl.selectedSegmentIndex;
     
-
+    if (selectedSegment == 0){
+        [self.labelTweets setHidden:NO];
+        [self.labelMedia setHidden:YES];
+        [self.labelFavorites setHidden:YES];
+    } else if (selectedSegment == 1){
+        [self.labelTweets setHidden:YES];
+        [self.labelMedia setHidden:NO];
+        [self.labelFavorites setHidden:YES];
+    } else if (selectedSegment == 2){
+        [self.labelTweets setHidden:YES];
+        [self.labelMedia setHidden:YES];
+        [self.labelFavorites setHidden:NO];
+    }
 }
+//- (IBAction)followButton:(UIButton *)sender {
+    
+//    self.followButton.class = [self.followButtonBar setHidden:NO];
+//    }
+//    self.followingBar.hidden = FALSE;
+//    self.followBar.hidden = TRUE;
+
+
 
 @end
