24f5e7818b599e900f5740c4d6eed7a96523bb3d
Eric Sze <you@example.com>
2015-07-24 21:26:41 -0400

    Twitter Interface Homework Finished

---
+++ TwitterUI/Twitter AutoLayout/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Twitter AutoLayout
+//
+//  Created by Eric Sze on 7/22/15.
+//  Copyright (c) 2015 Eric Sze. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
