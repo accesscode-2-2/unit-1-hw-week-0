c4f69e0dfb817a3bb37f4dd670b68f325e62291b
kaishajones <kaisha_jns@yahoo.com>
2015-10-02 06:57:01 -0400

    did most of layout: banner, profile, bio, etc.

--- TwitterUI/TwitterUI/ViewController.h
+++ TwitterUI/TwitterUI/ViewController.h
@@ -3,10 +3,12 @@
 //  TwitterUI
 //
 //  Created by Michael Kavouras on 7/19/15.
+//  Edited by Kaisha Jones on 7/23/15/.
 //  Copyright (c) 2015 Mike Kavouras. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
+
 
 @interface ViewController : UIViewController
 
--- TwitterUI/TwitterUI/ViewController.m
+++ TwitterUI/TwitterUI/ViewController.m
@@ -3,25 +3,186 @@
 //  TwitterUI
 //
 //  Created by Michael Kavouras on 7/19/15.
+//  Edited by Kaisha Jones on 7/23/15/.
 //  Copyright (c) 2015 Mike Kavouras. All rights reserved.
 //
 
 #import "ViewController.h"
+#import <QuartzCore/QuartzCore.h> // QuartzCore - added to round corners
+
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIImageView *topPhotoView;
+@property (weak, nonatomic) IBOutlet UIView *profilePicFrame;
+
+@property (weak, nonatomic) IBOutlet UIImageView *profilePicture;
+@property (weak, nonatomic) IBOutlet UIView *followButton;
+
+@property (weak, nonatomic) IBOutlet UIView *messageButton;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *tweetsMediaFavorites;
+
+@property (weak, nonatomic) IBOutlet UILabel *notLoadingTextLabel;
+@property (weak, nonatomic) IBOutlet UIButton *followButtonText;
+
+@property (weak, nonatomic) IBOutlet UILabel *followTextFieldLabel;
+@property (weak, nonatomic) IBOutlet UIImageView *followImageView;
+
+@property (weak, nonatomic) IBOutlet UIImageView *followingImageView;
+@property (weak, nonatomic) IBOutlet UIView *followBackgroundField;
+
+@property (weak, nonatomic) IBOutlet UILabel *numberOfFollowersLabel;
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+#pragma mark - (main) view controller
+
+
+
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    
+    //rounded corners for the profile pic
+    [self setupProfilePicture];
+    
+    //a border and rounded corners to the follow/following button
+    self.followFollowingButton.layer.borderWidth = 1.5;
+    self.followFollowingButton.layer.borderColor =[UIColor colorWithRed:0.424 green:0.710 blue:1 alpha:1].CGColor;
+    self.followFollowingButton.layer.cornerRadius = 5.0;
+    
+    //a border and rounded corners to the message me button
+    self.messageMeButton.layer.borderWidth = 1.5;
+    self.messageMeButton.layer.borderColor =[UIColor colorWithRed:0.424 green:0.710 blue:1 alpha:1].CGColor;
+    self.messageMeButton.layer.cornerRadius = 5.0;
+    
+    //color of follow button text
+    self.followTextField.textColor = [UIColor colorWithRed:0.424 green:0.710 blue:1 alpha:1];
+    
+    //original top image
+    self.topPhotoView.image = [self imageFromURLString:@"http://i.imgur.com/spJpxB0.jpg"];
+    //original profile pic
+    self.profilePic.image = [self imageFromURLString:@"https://pbs.twimg.com/profile_images/607610361433300992/OKxM0eFW_400x400.jpg"];
+
+}
+
+- (void)setupProfilePicture {
+    self.profilePicFrame.layer.cornerRadius = 5.0;
+    self.profilePic.layer.cornerRadius = 5.0;
+}
+
+-(UIImage *)imageFromURLString:(NSString *)string{
+    NSURL *url = [NSURL URLWithString:string];
+    NSData *data = [NSData dataWithContentsOfURL:url];
+    UIImage *image = [UIImage imageWithData:data];
+    return image;
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+//make the "Tweets aren't loading right now" text change when button is pushed
+- (IBAction)tweetsMediaFavsAction:(UISegmentedControl *)sender {
+    
+    if (self.tweetsMediaFavorites.selectedSegmentIndex == 0) {
+        
+        NSLog(@"tweets button tapped");
+        self.notLoadingText.text = @"Tweets aren't loading right now";
+        
+    } else if (self.tweetsMediaFavorites.selectedSegmentIndex == 1) {
+        
+        NSLog(@" media button tapped");
+        self.notLoadingText.text = @"Media isn't loading right now";
+        
+    }else if (self.tweetsMediaFavorites.selectedSegmentIndex == 2) {
+        
+        NSLog(@" favorites button tapped");
+        self.notLoadingText.text = @"Favorites aren't loading right now";
+        
+    }
+    
+}
+
+- (IBAction)followButtonTapped:(UIButton *)sender {
+    
+    NSInteger followersInt = [self.numberOfFollowersField.text intValue];
+    NSLog(@"%ld", (long)followersInt);
+    
+    if ([self.followTextField.text isEqual:@"Following"]) {
+        self.followTextField.textColor = [UIColor colorWithRed:0.424 green:0.710 blue:1 alpha:1];
+        self.followBackgroundField.backgroundColor = [UIColor whiteColor];
+        self.followTextField.text = @"Follow";
+        self.followImage.hidden = NO;
+        self.followingImage.hidden = YES;
+        
+        followersInt = followersInt - 1;
+        
+        NSString *newFollowers = [[NSNumber numberWithInteger:followersInt] stringValue];
+        NSLog(@"%@", newFollowers);
+        
+        self.numberOfFollowersField.text = newFollowers;
+        
+    }else {
+        
+        self.followBackgroundField.backgroundColor = [UIColor colorWithRed:0.424 green:0.710 blue:1 alpha:1];
+        self.followTextField.textColor = [UIColor whiteColor];
+        self.followTextField.text = @"Following";
+        self.followImage.hidden = YES;
+        self.followingImage.hidden = NO;
+        
+        followersInt = followersInt + 1;
+        
+        NSString *newFollowers = [[NSNumber numberWithInteger:followersInt] stringValue];
+        NSLog(@"%@", newFollowers);
+        
+        self.numberOfFollowersField.text = newFollowers;
+        
+        
+        
+        
+    }
+}
+
+//
+//////The following buttons will change the images but are not working to change the images back...
+//- (IBAction)topBackgroundImageButtonTapped:(UIButton *)sender {
+//    
+//    NSLog(@"top background image button tapped");
+//    
+//    UIImage *strawberries = [self imageFromURLString:@"
+//                             "];
+//    UIImage *costaRica = [self imageFromURLString:@"http://i.imgur.com/h420SYI.jpg"];
+//    
+//    if (self.topPhotoView.image == strawberries) {
+//        
+//        self.topPhotoView.image = costaRica;
+//        
+//    }else {
+//        NSLog(@"reached else");
+//        self.topPhotoView.image = strawberries;
+//    }
+//    
+//}
+
+//- (IBAction)profilePicButtonTapped:(UIButton *)sender {
+//    
+//    NSLog(@"profile pic button tapped");
+//    
+//    UIImage *curvedYellowFruit = [self imageFromURLString:@"http://ihatemygout.com/wp-content/uploads/2009/05/bananas-curved-yellow-fruit-300x212.jpg"];
+//    UIImage *underThePalms = [self imageFromURLString:@"https://pbs.twimg.com/profile_images/607610361433300992/OKxM0eFW_400x400.jpg"];
+//    
+//    if (self.profilePic.image == curvedYellowFruit){
+//        
+//        self.profilePic.image = underThePalms;
+//        
+//    }else {
+//        NSLog(@"reached else");
+//        self.profilePic.image = curvedYellowFruit;
+//        
+//    }
+//}
+
 
 @end
